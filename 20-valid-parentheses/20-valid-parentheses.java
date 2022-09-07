59d49967a8df19f0f4d29866a2a6f0ba42a3c157
Casey Carpenelli <yoyoman190@hotmail.com>
2022-09-07 17:35:28 -0500

    Time: 7 ms (5.99%), Space: 42 MB (52.31%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.isEmpty()) {
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++) {
+            if(stack.isEmpty() && (s.charAt(i) == ')' || s.charAt(i) == ']' || s.charAt(i) == '}')) {
+                return false;
+            } else {
+                if(!stack.isEmpty()) {
+                    if (
+                        stack.peek() == '(' && s.charAt(i) == ')'
+                    ) stack.pop(); else if (
+                        stack.peek() == '{' && s.charAt(i) == '}'
+                    ) stack.pop(); else if (
+                        stack.peek() == '[' && s.charAt(i) == ']'
+                    ) stack.pop(); else stack.add(s.charAt(i));
+                } else stack.add(s.charAt(i));
+                }
+            }
+        return stack.isEmpty();
+        }
+    }
